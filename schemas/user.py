97edb86c64d50d7ada9ba7181b66214f446b424f
erfan <erfan.arefmehr@gmail.com>
2023-02-07 12:04:08 +0330

    fix bugs

--- schemas/user.py
+++ schemas/user.py
@@ -3,11 +3,14 @@ from pydantic import BaseModel, Field
 
 # user base class
 class UserBase(BaseModel):
-    username: str = Field(max_length=20)
-    email: str = Field(regex="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}")
-    password: str = Field(max_length=16)
+    username: str = Field(default=..., max_length=20)
+    email: str
+    password: str = Field(default=..., max_length=16)
     
 # user display class
 class UserDisplay(BaseModel):
     username: str
     email: str
+    
+    class Config:
+        orm_mode = True
