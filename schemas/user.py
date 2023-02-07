4469b98e049584f4b02586f3db307c3b93d5e798
erfan <erfan.arefmehr@gmail.com>
2023-02-07 11:15:54 +0330

    create user display class

--- schemas/user.py
+++ schemas/user.py
@@ -6,3 +6,8 @@ class UserBase(BaseModel):
     username: str = Field(max_length=20)
     email: str = Field(regex="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}")
     password: str = Field(max_length=16)
+    
+# user display class
+class UserDisplay(BaseModel):
+    username: str
+    email: str
