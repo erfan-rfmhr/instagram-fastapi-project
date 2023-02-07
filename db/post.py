181978f659d23a934916c23dbc18c02596a1bdba
erfan <erfan.arefmehr@gmail.com>
2023-02-07 15:47:29 +0330

    create post method

---
+++ db/post.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Session
+
+from db.models import Post
+from schemas.post import PostBase
+
+
+def create_post(request: PostBase, db: Session):
+    post = Post(
+        image_url = request.image_url,
+        image_url_type = request.image_url_type,
+        caption = request.caption,
+        timestamp = datetime.now(),
+        user_id = request.user_id,
+    )
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return post
