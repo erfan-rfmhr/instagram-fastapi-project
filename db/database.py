6ad0161ba2c1327f84afbd9885c416b56f66c8fb
erfan <erfan.arefmehr@gmail.com>
2023-02-08 18:24:12 +0330

    update

--- db/database.py
+++ db/database.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 engin = create_engine("sqlite:///instagram.db", connect_args={"check_same_thread":False})
 Base = declarative_base()
 
+sessionlocal = sessionmaker(bind=engin)
+
 def get_db():
-    session = sessionmaker(bind=engin)()
+    session = sessionlocal()
     try:
         yield session
     finally:
