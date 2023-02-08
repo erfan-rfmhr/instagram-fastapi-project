55c3c09d43f19c608aa77b66980173e5fcae9592
erfan <erfan.arefmehr@gmail.com>
2023-02-08 11:51:10 +0330

    create login manager

---
+++ auth.py
@@ -0,0 +1,4 @@
+from fastapi_login import LoginManager
+
+SECRET_KEY = "7136c046c7752a8455eaeb38a9c6bc5aef26055863b50897cdd8faae170baa89"
+login_manager = LoginManager(SECRET_KEY, token_url="/user/login")
