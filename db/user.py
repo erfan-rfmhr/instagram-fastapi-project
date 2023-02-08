70ffca4e6f1a7296ec1857a48163766561920e61
erfan <erfan.arefmehr@gmail.com>
2023-02-08 18:24:29 +0330

    make user loader

--- db/user.py
+++ db/user.py
@@ -1,4 +1,7 @@
 from sqlalchemy.orm import Session
+
+from auth import login_manager
+from db.database import sessionlocal
 from db.hash import Hash
 from db.models import User
 from schemas.user import UserBase
@@ -15,5 +18,6 @@ def create_user(request: UserBase, db: Session):
     db.refresh(user)
     return user
 
-def get_user_by_username(username: str, db: Session):
+@login_manager.user_loader()
+def get_user_by_username(username: str, db: Session = sessionlocal()):
     return db.query(User).filter(User.username == username).first()
