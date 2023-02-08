7ec8a75ce35c214abcec15b17b058dd92a62d691
erfan <erfan.arefmehr@gmail.com>
2023-02-08 11:52:08 +0330

    login endpoint

--- routers/user.py
+++ routers/user.py
@@ -1,13 +1,34 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login.exceptions import InvalidCredentialsException
 from sqlalchemy.orm import Session
 
+from auth import login_manager
 from db import user as db_user
 from db.database import get_db
-from schemas.user import UserBase, UserDisplay
-from routers.utils import validate_email
+from db.hash import Hash
 from routers.exceptions import EmailValidationError
+from routers.utils import validate_email
+from schemas.user import UserBase, UserDisplay
 
 router = APIRouter(prefix="/user", tags=["user"])
+
+# login user
+@router.post('/login')
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db_user.get_user_by_username(form_data.username, db)
+    
+    if user is not None and Hash.verify(user.password, form_data.password):
+        access_token = login_manager.create_access_token(
+            data={"sub": user.username}
+        )
+        response = JSONResponse(content={"access_token": access_token, "token_type": "bearer"})
+        login_manager.set_cookie(response=response, token=access_token)
+        return response
+    else:
+        raise InvalidCredentialsException
+
 
 # create user router
 @router.post('/create', response_model=UserDisplay)
